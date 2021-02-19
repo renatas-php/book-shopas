0a73efdbc2db3d58b69326a1f04becdd23a6b956
renatas-php <rene.programuotojas@gmail.com>
2021-02-19 13:18:15 +0200

    to BookSeeder added genres and authors attach

--- database/seeders/BooksSeeder.php
+++ database/seeders/BooksSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 
 use App\Models\Book;
+use App\Models\Genre;
+use App\Models\Author;
 
 class BooksSeeder extends Seeder
 {
@@ -15,6 +17,11 @@ class BooksSeeder extends Seeder
      */
     public function run()
     {
-        Book::factory()->times(120)->create();
+        Book::factory()->times(120)->create()->each(function($book) {
+            $genres = Genre::all()->random(rand(1,3))->pluck('id');
+            $book->genres()->attach($genres);
+            $authors = Author::all()->random(rand(1,3))->pluck('id');
+            $book->authors()->attach($authors);
+        });
     }
 }
